111a3e9870f0f57eeabccd052cc5c08b6438b11e
Gaurav Ranjit <gvranjit@gmail.com>
2020-08-06 16:36:39 +0545

    Add Edit, Undo, Redo

--- commands/admin/edit.js
+++ commands/admin/edit.js
@@ -1,16 +1,20 @@
 const { client } = require("../../client.js");
 const { log } = require("../../utils/utils");
+const fs = require("fs");
 module.exports = {
      name: "edit",
      description: "Edits the last message written by CraftNepal",
      async execute(message, args) {
-          //check if the last message's author is a bot, and if yes, check if it is Craftnepal
-
           const messages = await message.channel.fetchMessages({ limit: 10 });
-
-          const filtered = messages.filter((msg) => {
+          const botMessages = messages.filter((msg) => {
                return msg.author.id === client().user.id;
           });
-          log(filtered + "was edited");
+          //this will edit the first message in the collection we filtered out.
+          const cache = botMessages.first();
+          fs.writeFile("cache.txt", cache, (err) => {
+               console.log(`Coudn't write File`);
+               console.log(err);
+          });
+          botMessages.first().edit(args.join(" "));
      },
 };
---
+++ commands/admin/redo.js
@@ -0,0 +1,31 @@
+const { client } = require("../../client.js");
+const { log } = require("../../utils/utils");
+const fs = require("fs");
+module.exports = {
+     name: "redo",
+     description: "Undos the last edited message by CraftNepal",
+     async execute(message, args) {
+          if (args[0] !== "edit") {
+               return;
+          }
+
+          const messages = await message.channel.fetchMessages({ limit: 10 });
+          const botMessages = messages.filter((msg) => {
+               return msg.author.id === client().user.id;
+          });
+          //this will edit the fi   rst message in the collection we filtered out.
+          const cache = botMessages.first();
+          fs.writeFile("cache.txt", cache, (err) => {
+               if (err) {
+                    console.log(`Coudn't write File`);
+               }
+          });
+          fs.readFile("redo-cache.txt", (err, data) => {
+               if (err) {
+                    console.log("Coudnt read from file.");
+               }
+               console.log(data.toString());
+               botMessages.first().edit(data.toString());
+          });
+     },
+};
